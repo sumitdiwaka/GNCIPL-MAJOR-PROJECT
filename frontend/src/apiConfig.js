f3eb58e02c6196a7d1534d3b0e35eeb92698a44d
unknown <sumitdiwakar476@gmai.com>
2025-10-03 21:53:46 +0530

    url

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,5 +1,2 @@
-// This line gets the base URL for your API from an environment variable if it exists,
-// otherwise it falls back to your local development server URL.
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
-
+const API_URL = import.meta.env.VITE_API_BASE_URL
 export default API_URL;
